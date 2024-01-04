81ccbcc4cee94ff7226a45017a816774f694ddeb
Camilo Ariza <camilo@60beans.com>
2024-01-03 23:21:40 -0500

    create gh deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,3 +129,37 @@ resource "aws_security_group" "docdb_sg" {
   }
 }
 
+## S3 for Terraform statefile
+
+provider "aws" {
+  region = "us-east-1"  # Your AWS region
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-state-bucket"  # Replace with your unique bucket name
+  versioning {
+    enabled = true
+  }
+
+  # Enable encryption
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name           = "my-terraform-lock-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
